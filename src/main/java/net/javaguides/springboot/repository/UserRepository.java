2675989a6e5107c0d7a2699b3086963f42cdf7f0
rajdileep <rajdileep@gmail.com>
2022-09-13 23:39:11 +0530

    Changes

--- src/main/java/net/javaguides/springboot/repository/UserRepository.java
+++ src/main/java/net/javaguides/springboot/repository/UserRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import net.javaguides.springboot.model.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User ,Long> {
+public interface UserRepository extends JpaRepository<User ,Long > {
 
 }
